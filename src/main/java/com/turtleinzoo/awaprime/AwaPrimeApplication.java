7a103fd468b52833c44f6482f6f2b3d7d1283248
TurtleDK <KongzCringe@gmail.com>
2026-04-17 21:00:35 +0200

    Initial commit

---
+++ src/main/java/com/turtleinzoo/awaprime/AwaPrimeApplication.java
@@ -0,0 +1,13 @@
+package com.turtleinzoo.awaprime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwaPrimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwaPrimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/turtleinzoo/awaprime/AwaPrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtleinzoo.awaprime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwaPrimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
